d13e7e564ce816c80caa98d20cb8782d5728bdfa
Divraj-M <2K21cse022@kiot.ac.in>
2025-04-13 14:35:28 +0530

    merge in divraj

--- src/test/java/com/Actions/ProductAction.java
+++ src/test/java/com/Actions/ProductAction.java
@@ -1,13 +1,19 @@
 package com.Actions;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
 import com.Pages.ProductPage;
 import com.Utilities.HelperClass;
 
 public class ProductAction {
 	ProductPage ProductPageLocator=null;
-	String strusername,strpassword;
+	String strusername,strpassword,urlexp;
 	
 	public ProductAction()
 	{
@@ -68,6 +74,88 @@ public class ProductAction {
 				+ "Your Shopping Cart is empty!    ";
 		String exp=ProductPageLocator.emptycart.getText();
 		System.out.println(exp);
+	}
+	public void addtowishlist()
+	{
+		ProductPageLocator.imgalbum3.click();
+		ProductPageLocator.addtowishlist.click();
+		ProductPageLocator.clickwishlist.click();
+	}
+	
+	public void urlwishlist()
+	{
+		System.out.println("Your wishlist URL for sharing:"+ProductPageLocator.urlforwishlist.getText());
+		this.urlexp=ProductPageLocator.urlforwishlist.getText();
+		ProductPageLocator.urlforwishlist.click();
+	}
+	public void urlautolist()
+	{
+		String exp=HelperClass.getDriver().getCurrentUrl();
+		urlexp.contains(exp);
+	}
+	
+	public void increaseqty()
+	{
+		ProductPageLocator.imgalbum3.click();
+		ProductPageLocator.increaseqty.clear();
+		ProductPageLocator.increaseqty.sendKeys("10");
+		ProductPageLocator.addtocartinqty.click();
+		ProductPageLocator.shoppingcart.click();
+		
+	}
+	public void updatedqtyinwishlist()  
+	{
+		List<WebElement> obj=HelperClass.getDriver().findElements(By.xpath("//table[@class=\"cart-total\"]//tr//td//span"));
+		for(WebElement a:obj)
+		{
+			System.out.println(a.getText());
+			
+		}
+	}
+	public void emailafriend()
+	{
+		ProductPageLocator.giftcardpage.click();
+	}
+	public void emailfriendbutton()
+	{
+		ProductPageLocator.emailfriendbtn.click();
+	}
+	
+	public void emailfriendsend(String frdemail,String myemail)
+	{
+		ProductPageLocator.friendemail.sendKeys(frdemail);
+		ProductPageLocator.myemail.clear();
+		ProductPageLocator.myemail.sendKeys(myemail);
+	}
+	public void sendmailfriend()
+	{
+		ProductPageLocator.sendmail.click();
+	}
+	public void successfulmailsend()
+	{
+		String act="Your message has been sent.";
+		System.out.println(ProductPageLocator.successfulsentmail.getText());
+		String exp=ProductPageLocator.successfulsentmail.getText();
+		Assert.assertEquals(act, exp);
+	}
+	public void unsuccessfulmailsend()
+	{
+		String act="Wrong email";
+		System.out.println(ProductPageLocator.invalidemailmsg.getText());
+		String exp=ProductPageLocator.invalidemailmsg.getText();
+		Assert.assertEquals(act, exp);
+	}
+	public void addtocartcheckboxs()
+	{
+		ProductPageLocator.addtocartcheckbox.click();
+		ProductPageLocator.addtocartbtnbelowchkbox.click();
+	}
+	
+	public void verifytextincart()
+	{
+		String textincart="3rd Album";
+		String exp=ProductPageLocator.assertcartwishlist.getText();
+		Assert.assertEquals(textincart, exp);
 	}
 	
 
--- src/test/java/com/Pages/ProductPage.java
+++ src/test/java/com/Pages/ProductPage.java
@@ -8,10 +8,10 @@ public class ProductPage
 	@FindBy(xpath="//li//a[@class=\"ico-login\"]")
 	public WebElement LoginButtonHomePage;
 	
-	@FindBy(xpath="//input[@class=\"email valid\"]")
+	@FindBy(id = "Email")
 	public WebElement EmailField;
 	
-	@FindBy(xpath="//input[@class=\"password valid\"]")
+	@FindBy(id = "Password")
 	public WebElement PasswordField;
 	
 	@FindBy(xpath="//input[@class=\"button-1 login-button\"]")
@@ -43,6 +43,59 @@ public class ProductPage
 	
 	@FindBy(xpath="//a[@class=\"product-name\"]")
 	public WebElement cartproductavailable;
+	
+	@FindBy(id="add-to-wishlist-button-53")
+	public WebElement addtowishlist;
+	
+	@FindBy(xpath="//ul/li[4]/a[@class=\"ico-wishlist\"]")
+	public WebElement clickwishlist;
+	
+	@FindBy(xpath="/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[3]/div[1]/div/div/div[1]/a")
+	public WebElement imgalbum3;
+	
+	@FindBy(xpath="//tbody/tr/td[4]/a")
+	public WebElement productnameinwishlist;
+	
+	@FindBy(xpath="/html/body/div[4]/div[1]/div[4]/div/div/div[2]/div[2]/p[2]/a")
+	public WebElement urlforwishlist;
+	
+	@FindBy(xpath="//input[@class=\"qty-input\"]")
+	public WebElement increaseqty;
+	
+	@FindBy(xpath = "//input[@class=\"button-1 add-to-cart-button\"]")
+	public WebElement addtocartinqty;
+	
+	@FindBy(xpath="/html/body/div[4]/div[1]/div[4]/div[3]/div/div/div[3]/div[2]/div/div[2]/h2/a")
+	public WebElement giftcardpage;
+	
+	@FindBy(xpath="//input[@class=\"button-2 email-a-friend-button\"]")
+	public WebElement emailfriendbtn;
+	
+	@FindBy(xpath="//input[@class=\"friend-email\"]")
+	public WebElement friendemail;
+	
+	@FindBy(xpath="//input[@class=\"your-email\"]")
+	public WebElement myemail;
+	
+	@FindBy(xpath="//input[@class=\"button-1 send-email-a-friend-button\"]")
+	public WebElement sendmail;
+	
+	@FindBy(xpath="/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[2]")
+	public WebElement successfulsentmail;
+	
+	@FindBy(xpath="//span[@class=\"field-validation-error\"]//span")
+	public WebElement invalidemailmsg;
+	
+	@FindBy(xpath="/html/body/div[4]/div[1]/div[4]/div/div/div[2]/div[1]/form/table/tbody/tr/td[2]/input")
+	public WebElement addtocartcheckbox;
+	
+	@FindBy(xpath="//input[@class=\"button-2 wishlist-add-to-cart-button\"]")
+	public WebElement addtocartbtnbelowchkbox;
+	
+	@FindBy(xpath="//a[@class=\"product-name\"]")
+	public WebElement assertcartwishlist;
+	
+	
 	
 	
 	
