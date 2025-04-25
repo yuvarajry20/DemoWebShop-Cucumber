0e9c1ddd5b6a1c4992e2331df8206e2bcaff2287
LAKSITHA29 <113997037+LAKSITHA29@users.noreply.github.com>
2025-04-25 09:14:46 +0530

    commit

--- src/test/java/com/Runner/runnerTestNG.java
+++ src/test/java/com/Runner/runnerTestNG.java
@@ -1,30 +1,30 @@
-package com.Runner;
-import org.testng.annotations.DataProvider;
+// package com.Runner;
+// import org.testng.annotations.DataProvider;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-@CucumberOptions(plugin = {"pretty",
-		"json:target/cucumber-reports/Cucumber.json",
-		"html:target/cucumber-reports/Cucumber.html",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-features={
-		// "src/test/resources/Features/Login.feature",
-		// "src/test/resources/Features/Product.feature",
-		"src/test/resources/Features/Books.feature",
-		"src/test/resources/Features/HomePage.feature",
-		//"src/test/resources/Features/Address.feature",
-		//"src/test/resources/Features/CheckOut.feature",
-		//"src/test/resources/Features/MyAccountSection.feature",
-		"src/test/resources/Features/SearchFunction.feature",
-		//"src/test/resources/Features/UserRegistration.feature",
-		"src/test/resources/Features/Review.feature",
-		"src/test/resources/Features/Tags.feature",
-		//"src/test/resources/Features/DigitalDownload.feature",
-},
-glue="com.Definitions")
-public class runnerTestNG extends AbstractTestNGCucumberTests {
-    @DataProvider(parallel = true)
-    public Object[][] scenarios() {
-    	return super.scenarios();
-	}
-}
+// import io.cucumber.testng.AbstractTestNGCucumberTests;
+// import io.cucumber.testng.CucumberOptions;
+// @CucumberOptions(plugin = {"pretty",
+// 		"json:target/cucumber-reports/Cucumber.json",
+// 		"html:target/cucumber-reports/Cucumber.html",
+// 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+// features={
+// 		// "src/test/resources/Features/Login.feature",
+// 		// "src/test/resources/Features/Product.feature",
+// 		"src/test/resources/Features/Books.feature",
+// 		"src/test/resources/Features/HomePage.feature",
+// 		//"src/test/resources/Features/Address.feature",
+// 		//"src/test/resources/Features/CheckOut.feature",
+// 		//"src/test/resources/Features/MyAccountSection.feature",
+// 		"src/test/resources/Features/SearchFunction.feature",
+// 		//"src/test/resources/Features/UserRegistration.feature",
+// 		"src/test/resources/Features/Review.feature",
+// 		"src/test/resources/Features/Tags.feature",
+// 		//"src/test/resources/Features/DigitalDownload.feature",
+// },
+// glue="com.Definitions")
+// public class runnerTestNG extends AbstractTestNGCucumberTests {
+//     @DataProvider(parallel = true)
+//     public Object[][] scenarios() {
+//     	return super.scenarios();
+// 	}
+// }
