a58edd12c07e221cacf974b1343fb521af666416
Divraj-M <2K21cse022@kiot.ac.in>
2025-04-28 20:47:23 +0530

    add the try catch

--- src/test/java/com/Actions/TricentisAction.java
+++ src/test/java/com/Actions/TricentisAction.java
@@ -5,24 +5,25 @@ import org.testng.Assert;
 
 import com.Pages.Tricentispage;
 import com.Utilities.HelperClass;
+import com.Utilities.LogManagers;
 
 public class TricentisAction {
-	Tricentispage trilocate = null;
+    Tricentispage trilocate = null;
 
     public TricentisAction() {
         this.trilocate = new Tricentispage();
         PageFactory.initElements(HelperClass.getDriver(), trilocate);
     }
     
-    public void tricentisimg()
-    {
-    	trilocate.tricentisimg.click();
-    }
-    public void assertpage()
-    {
-    	String act="Drive innovation at DevOps speed.";
-    	String exp=trilocate.asserttextinpage.getText();
-    	Assert.assertEquals(act, exp);
+    public void tricentisimg() {
+        trilocate.tricentisimg.click();
+        LogManagers.logInfo("Successfully clicked the Tricentis image.");
     }
 
+    public void assertpage() {
+        String act = "Drive innovation at DevOps speed.";
+        String exp = trilocate.asserttextinpage.getText();
+        Assert.assertEquals(act, exp, "The expected text does not match the actual text.");
+        LogManagers.logInfo("Successfully asserted the page text.");
+    }
 }
--- src/test/java/com/Definitions/TricentisstepDefinition.java
+++ src/test/java/com/Definitions/TricentisstepDefinition.java
@@ -7,27 +7,35 @@ import com.Utilities.LogManagers;
 import io.cucumber.java.en.*;
 
 public class TricentisstepDefinition {
-	TricentisAction objtri=new TricentisAction();
-	
+    TricentisAction objtri = new TricentisAction();
 
-@Given("the user is on the home page of the application")
-public void the_user_is_on_the_home_page_of_the_application() {
-	try {
-        HelperClass.openPage("https://demowebshop.tricentis.com/");
-    } catch (Exception e) {
-        LogManagers.logError("Failed to open the web application. Error: " + e.getMessage());
-        throw e;
+    @Given("the user is on the home page of the application")
+    public void the_user_is_on_the_home_page_of_the_application() {
+        try {
+            HelperClass.openPage("https://demowebshop.tricentis.com/");
+        } catch (Exception e) {
+            LogManagers.logError("Failed to open the web application. Error: " + e.getMessage());
+            throw e;
+        }
     }
-}
 
-@When("I click the Tricentis Academy image")
-public void i_click_the_tricentis_academy_image() {
-   objtri.tricentisimg();
-}
+    @When("I click the Tricentis Academy image")
+    public void i_click_the_tricentis_academy_image() {
+        try {
+            objtri.tricentisimg();
+        } catch (Exception e) {
+            LogManagers.logError("Failed to click the Tricentis Academy image. Error: " + e.getMessage());
+            throw e;
+        }
+    }
 
-@Then("the user should redirected to the tricentis webpage")
-public void the_user_should_redirected_to_the_tricentis_webpage() {
-    objtri.assertpage();
-
-}
+    @Then("the user should be redirected to the Tricentis webpage")
+    public void the_user_should_be_redirected_to_the_tricentis_webpage() {
+        try {
+            objtri.assertpage();
+        } catch (Exception e) {
+            LogManagers.logError("Failed to assert the Tricentis webpage. Error: " + e.getMessage());
+            throw e;
+        }
+    }
 }
