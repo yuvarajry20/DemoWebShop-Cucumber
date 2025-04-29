cd116bc26255841ac6214820b6aab92df326c49a
yuvarajry20 <2k21cse187@kiot.ac.in>
2025-04-29 19:27:19 +0530

    Added a Blog Scenario in the Customer Service Feature

--- src/test/java/com/Definitions/CustomerServiceStepDefinition.java
+++ src/test/java/com/Definitions/CustomerServiceStepDefinition.java
@@ -1,23 +1,49 @@
 package com.Definitions;
 
 import com.Actions.CustomerServiceAction;
-
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class CustomerServiceStepDefinition {
-	CustomerServiceAction customerServiceAction=new CustomerServiceAction();
-	
-	@When("I click the news link in customer service")
-	public void i_click_the_news_link_in_customer_service() {
-	    // Write code here that turns the phrase above into concrete actions
-		customerServiceAction.clickNewsLink();
-	}
+    CustomerServiceAction customerServiceAction = new CustomerServiceAction();
 
-	@Then("I can see the latest news for the website")
-	public void i_can_see_the_latest_news_for_the_website() {
-	    // Write code here that turns the phrase above into concrete actions
-	    String text=customerServiceAction.getLatestNewsText();
-	    System.out.print(text);
-	}
+    @When("I click the news link in customer service")
+    public void i_click_the_news_link_in_customer_service() {
+        customerServiceAction.clickNewsLink();
+    }
+
+    @Then("I can see the latest news for the website")
+    public void i_can_see_the_latest_news_for_the_website() {
+        String text = customerServiceAction.getLatestNewsText();
+        System.out.println(text);
+    }
+
+    @When("i click the blog link in customer service")
+    public void i_click_the_blog_link_in_customer_service() {
+        customerServiceAction.clickBlogLink();
+    }
+
+    @Then("i can see the blog title")
+    public void i_can_see_the_blog_title() {
+        String blogTitle = customerServiceAction.getBlogTitleText();
+        System.out.println(blogTitle);
+    }
+
+    @Then("i click the first blog")
+    public void i_click_the_first_blog() {
+        customerServiceAction.clickFirstBlog();
+    }
+
+    @Then("i can add an comment and click new comment")
+    public void i_can_add_an_comment_and_click_new_comment() {
+        customerServiceAction.addComment("This is a test comment.");
+        customerServiceAction.clickNewComment();
+    }
+
+    @Then("i can see the outcome")
+    public void i_can_see_the_outcome() {
+        String resultText = customerServiceAction.getResultText();
+        org.testng.Assert.assertEquals("Blog comment is successfully added.", resultText);
+        System.out.println(resultText);
+    }
 }
