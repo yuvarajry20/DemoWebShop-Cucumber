1a602a6f7292d7d57482ec15fd2e4c1ffccb111d
Divraj-M <2K21cse022@kiot.ac.in>
2025-04-29 21:05:02 +0530

    add two scenarios in the tricentis feature

--- src/test/java/com/Definitions/TricentisstepDefinition.java
+++ src/test/java/com/Definitions/TricentisstepDefinition.java
@@ -29,13 +29,54 @@ public class TricentisstepDefinition {
         }
     }
 
-@Then("the user should redirected to the tricentis webpage")
-public void the_user_should_redirected_to_the_tricentis_webpage() {
-	try {
-        objtri.assertpage();
-    } catch (Exception e) {
-        LogManagers.logError("Failed to assert the Tricentis webpage. Error: " + e.getMessage());
-        throw e;
+    @Then("the user should redirected to the tricentis webpage")
+    public void the_user_should_redirected_to_the_tricentis_webpage() {
+        try {
+            objtri.assertpage();
+        } catch (Exception e) {
+            LogManagers.logError("Failed to assert the Tricentis webpage. Error: " + e.getMessage());
+            throw e;
+        }
     }
-}
+
+    @When("click the upcoming webinars in the footer")
+    public void click_the_upcoming_webinars_in_the_footer() {
+        try {
+            objtri.upcomingevents();
+        } catch (Exception e) {
+            LogManagers.logError("Failed to click the upcoming webinars in the footer. Error: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    @Then("the user should see the webinars details")
+    public void the_user_should_see_the_webinars_details() {
+        try {
+            objtri.listoftitleinfilters();
+        } catch (Exception e) {
+            LogManagers.logError("Failed to see the webinars details. Error: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    @When("enter the keyword in the filter search box")
+    public void enter_the_keyword_in_the_filter_search_box() {
+        try {
+            objtri.searchthekeyword();
+        } catch (Exception e) {
+            LogManagers.logError("Failed to enter the keyword in the filter search box. Error: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    @Then("the user should see the upcoming partner event")
+    public void the_user_should_see_the_upcoming_partner_event() {
+        try {
+        	
+            objtri.unlockassert();
+        } catch (Exception e) {
+            LogManagers.logError("Failed to see the upcoming partner event. Error: " + e.getMessage());
+            throw e;
+        }
+    }
 }
