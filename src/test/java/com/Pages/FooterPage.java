aff75425375fd6c18cada4dde592170cffa51b82
LAKSITHA29 <laksitharavi@gmail.com>
2025-05-01 22:58:02 +0530

    commit

--- src/test/java/com/Pages/FooterPage.java
+++ src/test/java/com/Pages/FooterPage.java
@@ -8,7 +8,7 @@ public class FooterPage {
 	@FindBy(xpath="//a[contains(text(),\"nopCommerce\")]")
 	public WebElement nopFooterlink;
 	
-	@FindBy(xpath="//*[@id=\"en-page\"]/body/div[7]/section/div/div/div/div/div/div/div[1]/div/div[1]/h1")
+	@FindBy(xpath="//img[@title=\"nopCommerce\"]")
 	public WebElement nopCommerceText;
 	
 	@FindBy(xpath="//a[contains(text(),\"Sitemap\")]")
@@ -17,5 +17,22 @@ public class FooterPage {
 	@FindBy(xpath="(/html/body/div[4]/div[1]/div[4]/div/div/div[2]/div[2]/div[2]/ul/li[5]/a")
 	public WebElement AccessoriesLink;
 	
-
+	@FindBy(xpath="//a[contains(text(),\"Contact us\")]")
+	public WebElement ContactUsLink;
+	
+	@FindBy(xpath="//input[@class=\"fullname\"]")
+	public WebElement ContactName;
+	
+	@FindBy(xpath="//input[@class=\"email\"]")
+	public WebElement ContactEmail;
+	
+	@FindBy(xpath="//a[contains(text(),\"Contact us\")]")
+	public WebElement ContactEnquiry;
+	
+	@FindBy(xpath="//a[contains(text(),\"Contact us\")]")
+	public WebElement ContactSubmit;
+	
+	//@FindBy(xpath="//div[@class=\"result\"]")
+	@FindBy(xpath="//div[@class=\"page-body\"]")
+	public WebElement ConfirmText;
 }
