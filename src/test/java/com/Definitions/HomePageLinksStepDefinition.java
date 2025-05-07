1e9810a636efe570ead9eee746bd9d1d25ee5391
yuvarajry20 <2k21cse187@kiot.ac.in>
2025-05-07 09:15:33 +0530

    updated runner file

--- src/test/java/com/Definitions/HomePageLinksStepDefinition.java
+++ src/test/java/com/Definitions/HomePageLinksStepDefinition.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import com.Utilities.HelperClass;
 
@@ -18,14 +19,15 @@ public class HomePageLinksStepDefinition {
 	public void i_can_validate_the_test_links() {
 	    // Write code here that turns the phrase above into concrete actions
 	    try {
-//	    	List<WebElement> links = HelperClass.getDriver().findElements(By.tagName("a"));
-	    	List<WebElement> links = HelperClass.getDriver().findElements(By.xpath("//div[@class=\"column follow-us\"]//a"));
+	    	List<WebElement> links = HelperClass.getDriver().findElements(By.tagName("a"));
+//	    	List<WebElement> links = HelperClass.getDriver().findElements(By.xpath("//div[@class=\"column follow-us\"]//a"));
             System.out.println("Number of links: " + links.size());
             for (WebElement link : links) {
                 String url = link.getAttribute("href");
                 if (url != null && !url.isEmpty()) {
                     verifyUrl(url);
                 }
+                
             }
 	    } catch (Exception e) {
             System.out.println("An error occurred: " + e.getMessage());
@@ -46,6 +48,7 @@ public class HomePageLinksStepDefinition {
             } else {
                 System.out.println(url + " - " + connection.getResponseMessage() + " is a broken link.");
             }
+//            Assert.assertEquals(200, responseCode);
         } catch (IOException e) {
             System.out.println(url + " - is a broken link.");
         }
